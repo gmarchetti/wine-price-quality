f685ee4bcea078fd8bbed4746f5e34137579ffdb
Guilherme Marcetti <guilherme.marchetti@gmail.com>
2023-11-14 17:43:50 +0000

    refactored to use extracted getPrice function

--- models/parsers/ct-page-parser.js
+++ models/parsers/ct-page-parser.js
@@ -30,10 +30,8 @@ export default class SingleItemParser
 
     async getPriceFromSearchPage(page)
     {
-        let priceAsString = await page.$eval(".results-section > div:nth-child(2) > div:nth-child(2) > .product > .product-tile > .ct-inner-tile-wrap > .ct-tile-body > .ct-tile-bottom > .pwc-price-wrap > .price > .prices-wrapper > .sales > .value", (element) => {
-            return element.getAttribute("content")
-        })
-        return parseFloat(priceAsString)
+        let priceElement = await page.$(".results-section > div:nth-child(2) > div:nth-child(2) > .product > .product-tile > .ct-inner-tile-wrap > .ct-tile-body > .ct-tile-bottom > .pwc-price-wrap > .price")
+        return this.getPrice(priceElement)
     }
 
     async getPricesFromListingPage(page)
